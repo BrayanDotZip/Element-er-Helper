06b6ad5aa19a4d82d8796ccdb86b8b71af5e399c
BrayanDotZip <brayangzz0902@gmail.com>
2024-11-15 23:01:43 -0600

    - Implemented the main menu with input validation for choices (1-3). - Created the `learn()` function with a dictionary to store element facts (Hydrogen, Helium). - Added basic error handling for unrecognized input in both the main menu and learn section. - Started structure for the `quiz()` function with an introduction message.

--- Element-er_Helper.py
+++ Element-er_Helper.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 #Used to create a neat and clear terminal.
 def clear_terminal():
@@ -10,18 +11,18 @@ def clear_terminal():
 clear_terminal()
 
 #Create function for the introduction
-def menu():
-    print('''                                                                                                 
+def main():
+    print('''                                                                                                     
   Z                                                         
  ZU  U                                                T  UZ 
  ZV  UUUT                             XTUUUUUUUUUUTTTTT  UZ 
- ZV     V        Welcome To The       WY                 UZ 
+ ZV     V                             WY                 UZ 
  ZV     V                             WY                 UZ 
  ZV     VZZZZZZZZZZZZZZZZZZZZZZZZZZZZZWY                 UZ 
  ZV                                                      UZ 
- ZV                                                      UZ 
- ZV                  Element-er                          UZ 
- ZV                    Helper!                           UZ 
+ ZV                  Welcome To The                      UZ 
+ ZV                    Element-er                        UZ 
+ ZV                      Helper!                         UZ 
  ZV                                                      UZ 
  ZV                                                      UZ 
  ZPSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSOZ 
@@ -30,7 +31,41 @@ def menu():
          X                                             UZ 
          YXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXVZ
 
-                                                            
+                                                        
       ''') 
-    
-menu()
+    print('Here you have 3 Options!\n')
+    print('1. Learn - Choose from some of the coolest elements on the periodic table')
+    print('2. Quiz - Test your knowledge by identifying random elements!')
+    print('3. Combine - Choose and element to combine with another and see what new compound you create!\n')
+    choice = input('Please choose from options 1, 2, and 3: ')
+    if choice == '1':
+        learn()
+    elif choice == '2':
+        quiz()
+    elif choice == '3':
+        combine()
+    else:
+        clear_terminal()
+        print('\nInput not recognized!!! Please choose from the listed options.'.upper())
+        main()
+
+#Defining the learn function to use user input and dictionaries
+def learn():
+    clear_terminal()
+    elements = {
+        '1': 'Hydrogen\n-----\nAtomic number - 1\nState of matter - Gas',
+        '2': 'Helium - '
+    }
+    print('In this section you can choose from the list of elements and learn neat facts about them!\n')
+    choice = input('Please choose the number of the element you would like to learn about!\n\n1.Hydrogen   2. Helium\n\nEnter Here:')
+    if choice in elements:
+        clear_terminal()
+        print(elements[choice])
+    else:
+        print("Input not recognized!!! Please choose a valid option")
+
+def quiz():
+    clear_terminal()
+    print('In this section random elements will be presented to you. All you need to do is figure out the mystery element with 3 hints!')
+
+main()
