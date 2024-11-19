d0aa1289a4a965e3c47cac052327999f4ed21f60
BrayanDotZip <brayangzz0902@gmail.com>
2024-11-18 20:58:33 -0600

    - Introduced `Element` class using object-oriented programming (OOP) principles. - Added attributes to the `Element` class: name, symbol, atomic number, state of matter, description, and fun fact. - Created `display_element()` method to print detailed information about an element. - Updated the `learn()` function to instantiate `Element` objects (e.g., Hydrogen) and display their details. - Refined the main menu with loop functionality and input validation for continued user interaction. - Enhanced user experience with improved element display and navigation options in the learn section.

--- Element-er_Helper.py
+++ Element-er_Helper.py
@@ -1,6 +1,23 @@
 import os
 import random
 
+class Element:
+  def __init__(self, name, symbol, atomic_number, state_of_matter, description, fun_fact):
+    self.name = name
+    self.symbol = symbol
+    self.atomic_number = atomic_number
+    self.state_of_matter = state_of_matter
+    self.description = description
+    self.fun_fact = fun_fact
+  def display_element(self):
+    clear_terminal()
+    print(f"Element: {self.name}")
+    print(f"Symbol: {self.symbol}")
+    print(f"Atomic Number: {self.atomic_number}")
+    print(f"State of matter: {self.state_of_matter}\n")
+    print(f"Here's a short description about {self.name}: {self.description}\n")
+    print(f'Fun fact! {self.fun_fact}')
+
 #Used to create a neat and clear terminal.
 def clear_terminal():
     if os.name == 'nt':
@@ -12,7 +29,8 @@ clear_terminal()
 
 #Create function for the introduction
 def main():
-    print('''                                                                                                     
+    while True:
+        print('''                                                                                                     
   Z                                                         
  ZU  U                                                T  UZ 
  ZV  UUUT                             XTUUUUUUUUUUTTTTT  UZ 
@@ -30,39 +48,52 @@ def main():
          W                                             UZ 
          X                                             UZ 
          YXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXVZ
-
-                                                        
+                                                         
       ''') 
-    print('Here you have 3 Options!\n')
-    print('1. Learn - Choose from some of the coolest elements on the periodic table')
-    print('2. Quiz - Test your knowledge by identifying random elements!')
-    print('3. Combine - Choose and element to combine with another and see what new compound you create!\n')
-    choice = input('Please choose from options 1, 2, and 3: ')
-    if choice == '1':
-        learn()
-    elif choice == '2':
-        quiz()
-    elif choice == '3':
-        combine()
-    else:
-        clear_terminal()
-        print('\nInput not recognized!!! Please choose from the listed options.'.upper())
-        main()
+        print('Here you have 3 Options!\n')
+        print('1. Learn - Choose from some of the coolest elements on the periodic table')
+        print('2. Quiz - Test your knowledge by identifying random elements!')
+        print('3. Combine - Choose and element to combine with another and see what new compound you create!\n')
+        
+        choice = input('Please choose from options 1, 2, and 3: ')
 
-#Defining the learn function to use user input and dictionaries
+        if choice == '1':
+            learn()
+            break
+        elif choice == '2':
+            quiz()
+            break
+        elif choice == '3':
+            combine()
+            break
+        else:
+            clear_terminal()
+            print('\nInput not recognized!!! Please choose from the listed options.'.upper())
+
+
+#
 def learn():
     clear_terminal()
+    hydrogen = Element("Hydrogen", "H", "1", "Gas", "It is the lightest element and, at standard conditions, is a gas of diatomic molecules with the formula H 2, sometimes called dihydrogen, but more commonly called hydrogen gas, molecular hydrogen or simply hydrogen. It is colorless, odorless, non-toxic, and highly combustible.", "It is the best element in the universe!")
+ 
     elements = {
-        '1': 'Hydrogen\n-----\nAtomic number - 1\nState of matter - Gas',
-        '2': 'Helium - '
+        '1': hydrogen,
     }
-    print('In this section you can choose from the list of elements and learn neat facts about them!\n')
-    choice = input('Please choose the number of the element you would like to learn about!\n\n1.Hydrogen   2. Helium\n\nEnter Here:')
-    if choice in elements:
+    
+    while True:
         clear_terminal()
-        print(elements[choice])
-    else:
-        print("Input not recognized!!! Please choose a valid option")
+        print('In this section you can choose from the list of elements and learn neat facts about them!\n')
+        choice = input('Please choose the number of the element you would like to learn about!\n\n1.Hydrogen   2. Helium\n\nEnter Here:')
+
+        if choice in elements:
+            clear_terminal()
+            elements[choice].display_element()
+
+            go_back = input("\nPress 1 to go back and select another element!")
+            if go_back == 1:
+              break
+        else:
+            print("Input not recognized!!! Please choose a valid option")
 
 def quiz():
     clear_terminal()
